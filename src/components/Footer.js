332682bfa6d987858bd79383febadb825ba11104
benjp009 <patin.benjamin@gmail.com>
2022-02-04 10:29:22 +0100

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <h3 className="footer-h3">
+      <span>Created By </span>
+      <a className="footer-link" href="https://github.com/thecodechaser">Benjamin Patin</a>
+    </h3>
+  </footer>
+);
+
+export default Footer;
