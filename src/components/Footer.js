997f4cac318df30b3f47c0debe582d432a4637c0
benjp009 <patin.benjamin@gmail.com>
2022-02-04 13:52:31 +0100

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer className="footer">
     <h3 className="footer-h3">
       <span>Created By </span>
-      <a className="footer-link" href="https://github.com/thecodechaser">Benjamin Patin</a>
+      <a className="footer-link" href="https://github.com/benjp009">Benjamin Patin</a>
     </h3>
   </footer>
 );
