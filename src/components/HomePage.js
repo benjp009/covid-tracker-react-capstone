2a96377357fe343a8f3d0ec763b48b666e8246e4
benjp009 <patin.benjamin@gmail.com>
2022-02-05 14:37:23 +0100

    add search

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Banner from '../assets/Banner.jpg';
 import { fetchDataApi } from '../redux/covid19Data';
@@ -7,6 +7,7 @@ import Header from './Header';
 import Footer from './Footer';
 
 const HomePage = () => {
+  const [searchText, setSearchText] = useState('');
   const heading = 'Worldwide COVID19 statistics';
   const covid19Data = useSelector((state) => state.covid19Data.countriesData);
   const globalData = useSelector((state) => state.covid19Data.globalData);
@@ -14,6 +15,11 @@ const HomePage = () => {
   useEffect(() => {
     dispatch(fetchDataApi());
   }, [dispatch]);
+
+  const filteredData = covid19Data.filter((item) => Object.keys(item).some((key) => item[key]
+    .toString()
+    .toLowerCase()
+    .includes(searchText.toLocaleLowerCase())));
 
   return (
     <>
@@ -33,12 +39,12 @@ const HomePage = () => {
         </div>
         <div className="search-bar">
           <h3 className="search-bar-h3">Search by country name</h3>
-          <input type="text" placeholder="Search" className="search-bar-input" />
+          <input value={searchText} onChange={(event) => setSearchText(event.target.value)} type="text" placeholder="Search" className="search-bar-input" />
           <h4 className="search-bar-h4">STATS BY COUNTRY</h4>
         </div>
         <div className="categories">
           {
-          covid19Data.map((data) => (
+          filteredData.map((data) => (
             <Countries key={data.ID} data={data} />
           ))
       }
